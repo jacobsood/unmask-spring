2d180dc281b4b8d45e5598cb889a4ea84d483041
Jacob Sood <hsoo6373@uni.sydney.edu.au>
2020-10-26 01:34:39 +1100

    Added hibernate annotations. Implementing Serializable

--- src/main/java/au/usyd/elec5619/domain/Article.java
+++ src/main/java/au/usyd/elec5619/domain/Article.java
@@ -1,10 +1,37 @@
 package au.usyd.elec5619.domain;
 
-public class Article {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Article")
+public class Article implements Serializable {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "Id")
+    private long id;
+
+    @Column(name = "Title", nullable = false)
     private String title;
+
+    @Column(name = "Source")
     private String source;
+
+    @Column(name = "Text")
     private String text;
+
+    @Column(name = "AudioUrl", nullable = false)
     private String audioUrl;
+
+    public long getId() {
+        return id;
+    }
     
     public String getTitle() {
         return title;
@@ -21,6 +48,10 @@ public class Article {
     public String getAudioUrl() {
         return audioUrl;
     }
+
+    public void setId(long id) {
+        this.id = id;
+    } 
 
     public void setTitle(String title) {
         this.title = title;
