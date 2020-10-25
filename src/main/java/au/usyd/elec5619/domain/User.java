63844eff8b9cad7703d6daa18be14a02229b699f
Jacob Sood <hsoo6373@uni.sydney.edu.au>
2020-10-26 06:20:18 +1100

    Added User and Preference Entity template

---
+++ src/main/java/au/usyd/elec5619/domain/User.java
@@ -0,0 +1,16 @@
+/* 
+
+    Define what's being stored. Look at Article.java, Country.java or Tag.java for assistance
+
+    Relationship:
+    1. Many-To-Many relationship with Articles (article favourites list)
+    2. Many-To-Many relationship with Stories (story favourties list)
+    3. Many-To-Many relationship with articles and stories (History list)
+    4. (More if needed)
+
+    See how many-to-many is implemented in Article.java (and Tag.java) - with Article being the owner. 
+    or at https://www.baeldung.com/jpa-many-to-many
+
+    Ensure tests are also being written for all methods
+
+*/
