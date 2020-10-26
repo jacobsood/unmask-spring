52bed6eefaddee9a122721308d569fd07668e31d
Jacob Sood <hsoo6373@uni.sydney.edu.au>
2020-10-26 18:13:22 +1100

    Added all changes to share working environment

---
+++ src/test/java/au/usyd/elec5619/web/ArticleControllerTest.java
@@ -0,0 +1,12 @@
+package au.usyd.elec5619.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArticleControllerTest {
+    @BeforeAll
+    void setUp() {
+        
+    }
+}
