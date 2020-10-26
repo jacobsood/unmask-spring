d49c2b4ee16ac763d3211e76913cdc39039ada48
Jacob Sood <hsoo6373@uni.sydney.edu.au>
2020-10-26 18:10:31 +1100

    Updated controller to show sample jsp and html redirection

--- src/main/java/au/usyd/elec5619/web/HelloController.java
+++ src/main/java/au/usyd/elec5619/web/HelloController.java
@@ -5,17 +5,13 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Created by Jacob on 20/10/20
- */
-
 @RequestMapping
 @Controller
 public class HelloController {
     @RequestMapping(method = RequestMethod.GET)
     public String printHello(ModelMap model) {
-        // "message" is an attribute in "hello.jsp"
-        model.addAttribute("message", "Hello Spring MVC Framework!");
-        return "hello";
+		return "hello"; // for jsp
+		// for html 
+        //return "redirect:/vue/main.html";
     }
 }
