b23d41b0b25ed5d53ce918c8be3ce56bb48b8d30
Jacob Sood <hsoo6373@uni.sydney.edu.au>
2020-10-28 06:33:17 +1100

    Updated view. Controller now using @GetMapping

--- src/main/java/au/usyd/elec5619/web/HelloController.java
+++ src/main/java/au/usyd/elec5619/web/HelloController.java
@@ -4,14 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RequestMapping
 @Controller
 public class HelloController {
-    @RequestMapping(method = RequestMethod.GET)
-    public String printHello(ModelMap model) {
-		return "hello"; // for jsp
-		// for html 
-        //return "redirect:/vue/main.html";
+    @GetMapping("/") 
+    public String home(ModelMap model) {
+        return "/vue/main.html";
+    }
+
+    @GetMapping("login") 
+    public String login(ModelMap model) {
+        return "/WEB-INF/view/hello.jsp";
     }
 }
