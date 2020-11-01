47360b6e7e74d476a9597654ef0c2e5d00f69276
Jacob Sood <hsoo6373@uni.sydney.edu.au>
2020-11-01 13:42:00 +1100

    Changed column names

--- src/main/java/au/usyd/elec5619/service/database/DatabaseUserManager.java
+++ src/main/java/au/usyd/elec5619/service/database/DatabaseUserManager.java
@@ -44,10 +44,9 @@ public class DatabaseUserManager implements UserManager {
     @Override
     public List<Article> getFavouriteArticlesByUserId(Long id) {
         String hql = 
-                    "SELECT a " +
                     "FROM Article a " +
                     "JOIN UserFavourite uf " +
-                    "WHERE uf.user_id = :id";
+                    "WHERE uf.u_id = :id";
         TypedQuery<Article> queryList = this.sessionFactory.getCurrentSession().createQuery(hql, Article.class);
         List<Article> articleList = queryList.setParameter("id", id).getResultList();
         return articleList;
@@ -56,13 +55,9 @@ public class DatabaseUserManager implements UserManager {
     @Override
     public List<Article> getViewedArticlesByUserId(Long id) {
         String hql = 
-                    "SELECT a " +
-                    "FROM User AS u " +
-                    "JOIN UserHistory AS uh " +
-                        "ON u.id = uh.user_id " +
-                    "JOIN Article AS a " +
-                        "ON a.id = uh.article_id " +
-                    "WHERE u.id = :id";
+                    "FROM Article a " +
+                    "JOIN UserHistory uh " +
+                    "WHERE uh.u_id = :id";
         TypedQuery<Article> queryList = this.sessionFactory.getCurrentSession().createQuery(hql, Article.class);
         List<Article> articleList = queryList.setParameter("id", id).getResultList();
         return articleList;
