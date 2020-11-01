1ae610995c54a2ff18a23b7dcf1e574427580de4
Jacob Sood <hsoo6373@uni.sydney.edu.au>
2020-11-01 11:40:11 +1100

    reallocated and removed deprecated services

--- src/main/java/au/usyd/elec5619/service/RegistrationManager.java
+++
@@ -1,9 +0,0 @@
-package au.usyd.elec5619.service;
-
-import au.usyd.elec5619.domain.User;
-
-public interface RegistrationManager {
-    // registration
-
-    public void registration(User user);
-}
