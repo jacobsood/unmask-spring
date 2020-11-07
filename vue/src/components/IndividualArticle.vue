f94e2a138425dbb2825e1e349c14c9712b1ba330
hsoo6373 <hsoo6373@uni.sydney.edu.au>
2020-11-08 10:30:15 +1100

    Added typed.js and updated scss

--- vue/src/components/IndividualArticle.vue
+++ vue/src/components/IndividualArticle.vue
@@ -10,14 +10,18 @@
           <span :key="tag">{{ tag }}</span>
         </template>
       </p>
-      
+      <div class="typedjs">
+        <vue-typed-js :strings="[article.text]">
+          <p class="typing"></p>
+        </vue-typed-js>
+      </div>
+      <svg v-if="!heart" @click="like" width="1.2em" height="1.2em" viewBox="0 0 16 16" class="bi bi-heart" fill="red" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"></path>
+      </svg>
+      <svg v-if="heart" @click="like" width="1.2em" height="1.2em" viewBox="0 0 16 16" class="bi bi-heart-fill" fill="red" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"></path>
+      </svg>
     </div>
-    <svg v-if="!heart" @click="like" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-      <path fill-rule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"></path>
-    </svg>
-    <svg v-if="heart" @click="like" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-      <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"></path>
-    </svg>
   </div>
 </template>
 
@@ -64,31 +68,72 @@ export default {
 
   .article-container {
     width: 60vw;
-    min-height: 50vh;
+    height: 50vh;
 
     border: 2px solid red;
     text-align: center;
 
     h1 {
-      font-size: 8vh;
+      font-size: 9vh;
+    }
+
+    p {
+      font-size: 3.5vh;
     }
     
     .title, .author {
-      background: none;
       position: relative;
       left: -50%;
-      top: -100%;
+      top: 25%;
+    }
+
+    .title {
+      background: none;
+    }
+    .author {
+
+      color: red;
     }
 
     .country {
+      font-size: 4vh;
       background: none;
       position: relative;
       right: -40%;
       transform: rotate(-90deg);
+      top: -42%;
     }
-  }
-  .bi {
-    margin-left: 10px;
+
+    .tags {
+      background: none;
+      position: relative;
+      right: -50%;
+      top: 30%;
+      span {
+        padding: 0 0.5% 0 0.5%;
+      }
+    }
+
+    .bi {
+      position: relative;
+      right: -65%;
+      top: 66vh;
+    }
+
+    .typedjs {
+      background: none;
+      position: relative;
+      transform: rotate(-20deg);
+      left: 15%;
+      top: -30%;
+      width: 70%;
+      height: 60%;
+    }
+
+    .typing {
+      font-family: GTSectraFine;
+      font-size: 2.5vh;
+    }
   }
 }
 </style>
