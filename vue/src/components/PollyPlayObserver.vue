f4951627c731d9feb7c55c6a3ac497fc4008eb57
hsoo6373 <hsoo6373@uni.sydney.edu.au>
2020-11-07 21:33:16 +1100

    Vue observer components to detect when to play/pause the audio

---
+++ vue/src/components/PollyPlayObserver.vue
@@ -0,0 +1,26 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    export default {
+        name: 'PollyPlayObserver',
+        data: function() {
+            return {
+                observer: null,
+            };
+        },
+        mounted: function() {
+            let option = {
+                rootMargin: '0% 0px -40% 0px',
+            };
+            this.observer = new IntersectionObserver(([entry]) => {
+                if (entry && entry.isIntersecting) {
+                    this.$emit('intersect');
+                }
+            }, option);
+            
+            this.observer.observe(this.$el);
+        }
+    }
+</script>
