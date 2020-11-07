548f8d168ca8a01cf5bcfae0b0a97c92892ba03e
hsoo6373 <hsoo6373@uni.sydney.edu.au>
2020-11-08 08:18:26 +1100

    Removed unnecessary elements and css

--- vue/src/components/NewStory.vue
+++ vue/src/components/NewStory.vue
@@ -5,28 +5,6 @@
         <h4>Unmask - <span>Stories Untold</span></h4>  
       </router-link>
     </header>
-    <div class="left-side-bar side-bar">
-      <router-link :to="{ name: 'landing'}">
-        Preference
-      </router-link>
-      <router-link :to="{ name: 'landing'}">
-        History
-      </router-link>
-    </div>
-    <div class="right-side-bar side-bar">
-      <router-link :to="{ name: 'landing'}">
-        Stories
-      </router-link>
-      <router-link :to="{ name: 'landing'}">
-        Search
-      </router-link>
-      <router-link :to="{ name: 'landing'}">
-        Articles
-      </router-link>
-      <router-link :to="{ name: 'landing'}">
-        New Story
-      </router-link>
-    </div>
     
     <form class="article-form">
         <h1 id="page-title">New Story</h1>
@@ -40,30 +18,16 @@
         <textarea id="new-tag" rows="10" />
         <button id="publish-button">Publish</button>
     </form>
-
-    <!-- Article was here -->
-    <observer @intersect="intersecting"></observer>
   </div>
 </template>
 
 <script>
-import Observer from './Observer';
-
 export default {
   name: 'NewStory',
   data: function() {
     return {
       
     }
-  },
-  components: {
-    Observer
-  },
-  methods: {
-    
-  },
-  computed: {
-    
   },
 }
 </script>
@@ -84,26 +48,6 @@ header {
   width: 100%;
 
   span {
-    color: red;
-  }
-}
-
-.side-bar {
-  height: 100%;
-  position: fixed;
-  z-index: 1;
-  top: 0;
-  overflow: hidden;
-  text-orientation: upright;
-  writing-mode: vertical-lr;
-
-  display: flex;
-  align-content: space-around;
-  justify-content: space-around;
-
-  padding: 0 3px 0 3px;
-
-  a {
     color: red;
   }
 }
@@ -159,6 +103,4 @@ header {
     text-align: center;
     font-family: "CustomY78";
 }
-
-
 </style>
