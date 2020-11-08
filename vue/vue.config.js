0c076586578b1632d834598a08eb5880f3f845e9
Ray-inthebox <47978716+Ray-inthebox@users.noreply.github.com>
2020-11-08 14:34:52 +0800

    Add files via upload
    add proxy

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  devServer: {
+    proxy: {
+      '/api': {
+        target: `http://localhost:8080`,
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api' : ''
+        }
+      }
+    }
+}
 }
