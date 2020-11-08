a5795c9b3b76ad1c94c8b66ec954b243a2bdc0ed
Mark <alfaru@live.ru>
2020-11-08 18:56:13 +1100

    Field verification added in NewStory

--- vue/src/components/NewStory.vue
+++ vue/src/components/NewStory.vue
@@ -10,16 +10,16 @@
         <h1 id="page-title">New Story</h1>
 
         <label for="input-fields">Title</label>
-        <textarea class="input-fields" rows="10" v-model="titleData" />
+        <textarea v-bind:class="{ 'input-fields': !titleHasError, 'error-input-fields': titleHasError }" rows="10" v-model="titleData" />
         <label for="input-fields">Auther</label>
-        <textarea class="input-fields" rows="10" v-model="autherData" />
+        <textarea v-bind:class="{ 'input-fields': !autherHasError, 'error-input-fields': autherHasError }" rows="10" v-model="autherData" />
         <label for="input-fields">Country</label>
-        <textarea class="input-fields" rows="10" v-model="countryData" />
+        <textarea v-bind:class="{ 'input-fields': !countryHasError, 'error-input-fields': countryHasError }" rows="10" v-model="countryData" />
 
         <label for="new-text">Text</label>
-        <textarea id="new-text" rows="10" v-model="textData" />
+        <textarea v-bind:class="{ 'new-text': !textHasError, 'error-new-text': textHasError }" rows="10" v-model="textData" />
 
-        <textarea class="new-tag" rows="10" v-for="(tag, index) in tagData" v-model="tag.tagText" :key="index" />
+        <textarea v-bind:class="{ 'new-tag': isTagsOk(), 'error-new-tag': !isTagsOk() }" rows="10" v-for="(tag, index) in tagData" v-model="tag.tagText" :key="index" />
         <!-- Plus button -->
         <svg @click="newTagField" width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-plus-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
@@ -43,13 +43,54 @@ export default {
           { tagText: "" },
       ],
       autherData: "",
-      countryData: ""
+      countryData: "",
+      titleHasError: false,
+      autherHasError: false,
+      countryHasError: false,
+      textHasError: false,
+      tagHasError: false,
+      wasPageSubmitted: false,
+      errorsOnPage: false
     }
   },
   methods: {
     publish(){ //method run on button click
-        console.log(this.titleData, this.textData, this.tagData);
+      this.wasPageSubmitted = true;//records submit attempt
+      this.errorsOnPage = false;
+      if(this.titleData.length < 3){
+        this.titleHasError = true;
+        this.errorsOnPage = true;
+      } else {this.titleHasError = false}
+      if(this.autherData.length < 3){
+        this.autherHasError = true;
+        this.errorsOnPage = true;
+      } else {this.autherHasError = false}
+      if(this.countryData.length < 2){
+        this.countryHasError = true;
+        this.errorsOnPage = true;
+      } else {this.countryHasError = false}
+      if(this.textData.length < 5){
+        this.textHasError = true;
+        this.errorsOnPage = true;
+      } else {this.textHasError = false}
+      //Array of non-empty tags
+      let tagsToSubmit = this.tagData.filter((tag) => tag.tagText.length > 0);
+      if(tagsToSubmit.length < 1){
+        this.errorsOnPage = true;
+      }
+      //////// If errorsOnPage is FALSE here then form can be submitted
+      if(!this.errorsOnPage){
+        console.log("Clear to submit");
+      }
     },
+
+    isTagsOk(){
+      let tags = this.tagData.filter((tag) => tag.tagText.length > 0);
+      if(tags.length < 1 && this.wasPageSubmitted){
+        return false;
+      } else {return true}
+    },
+
     newTagField(){
         this.tagData.push({ tagText: "" });
     }
@@ -109,7 +150,7 @@ header {
     max-height: 2vw;
     font-size: 1rem;
 }
-#new-text {
+.new-text {
     background-color: rgb(41, 40, 40);
     width: 40vw;
     min-height: 20vw;
@@ -121,6 +162,13 @@ header {
     width: 20vw;
     margin-left: 5vw;
 }
+.error-new-tag {
+    background-color: rgb(70, 0, 0);
+    max-height: 2vw;
+    width: 20vw;
+    margin-left: 5vw;
+}
+
 #publish-button {
     background-color: rgb(41, 40, 40);
     margin-left: 35vw;
@@ -131,5 +179,17 @@ header {
 #page-title{
     text-align: center;
     font-family: "CustomY78";
+}
+.error-input-fields{
+  background-color: rgb(70, 0, 0);
+  width: 40vw;
+  max-height: 2vw;
+  font-size: 1rem;
+}
+.error-new-text{
+  background-color: rgb(70, 0, 0);
+  width: 40vw;
+    min-height: 20vw;
+    font-size: 1rem;
 }
 </style>
