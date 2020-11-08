1687e1ae97a94a52d51f3249c1bf850b8cc023e7
Xue Zhao <402949621@qq.com>
2020-11-08 10:04:27 +0800

    Added comments

--- vue/src/components/IndividualArticle.vue
+++ vue/src/components/IndividualArticle.vue
@@ -21,20 +21,30 @@
       <svg v-if="heart" @click="like" width="1.2em" height="1.2em" viewBox="0 0 16 16" class="bi bi-heart-fill" fill="red" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"></path>
       </svg>
+      <svg @click="showComments=!showComments" 
+        style="fill:white;" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
+        <path d="M17 3c0-.55-.45-1-1-1H2c-.55 0-1 .45-1 1v11c0 .55.45 1 1 1h11.5l3.5 3V3zm-4 8H5V9h8v2zm0-3H5V6h8v2z"/>
+      </svg>
     </div>
+    <Comments v-bind:show-dialog="showComments"
+      v-on:close-comments="showComments=!showComments"></Comments>
   </div>
 </template>
 
 <script>
 import PollyAudio from './PollyAudio';
+import Comments from './Comments';
+
 export default {
   name: 'IndividualArticle',
   components: {
     PollyAudio,
+    Comments,
   },
   data() {
     return {
-      heart: 0
+      heart: 0,
+      showComments: false
     }
   },
   props: {
@@ -50,7 +60,7 @@ export default {
         this.heart = 0;
       }
     }
-  }
+  },
 }
 </script>
 
