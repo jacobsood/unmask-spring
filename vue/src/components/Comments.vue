b5ec8873935e34db23d711d55e54b78fb884baf7
Xue Zhao <402949621@qq.com>
2020-11-08 10:15:59 +0800

    Updated like button

--- vue/src/components/Comments.vue
+++ vue/src/components/Comments.vue
@@ -14,7 +14,9 @@
                 </div>
                 <button id="button-next-comment" @click="nextComment">＞</button>
             </div>
-            <img id="button-up-vote" src="https://www.flaticon.com/svg/static/icons/svg/1152/1152935.svg" />
+            <svg style="fill:white;" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
+                <path d="M1 21h4V9H1v12zm22-11c0-1.1-.9-2-2-2h-6.31l.95-4.57.03-.32c0-.41-.17-.79-.44-1.06L14.17 1 7.59 7.59C7.22 7.95 7 8.45 7 9v10c0 1.1.9 2 2 2h9c.83 0 1.54-.5 1.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-1.91l-.01-.01L23 10z"/>
+            </svg>
         </div>
         <button style="font-size: xx-large; color: white;" @click="$emit('close-comments')">×</button>
         </div>
