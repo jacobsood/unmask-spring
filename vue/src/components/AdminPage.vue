f5b163ef40169153014ee1329e91ab1b4ff8507f
yzho0516 <yzho0516@users.noreply.github.com>
2020-11-09 03:45:50 +1100

    Update AdminPage.vue
    Get/Post Method

--- vue/src/components/AdminPage.vue
+++ vue/src/components/AdminPage.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
+  <div class="admin-page">
     <header>
       <nav>
         <h1>Unmask Administration</h1>
       </nav>
     </header>
-    <div calss="listArticle" v-if="showArticle == true">
+    <div class="listArticle" v-if="showArticle == true">
       <div class="card" v-for="article in articles" :key="article.id">
         <div class="container">
           <div>
@@ -13,16 +13,76 @@
               <b>{{ article.id }} - {{ article.title }}</b>
             </h5>
             <p class="card-text">{{ article.text }}</p>
-            <p calss="card-text">{{ article.tags }}</p>
+            <p class="card-text">{{ article.tags }}</p>
           </div>
         </div>
       </div>
       <button v-on:click="showArticle = false">Go Back</button>
-      <button v-on:click="listByTag">Edit</button>
+      <button v-on:click="createArticle = true">Create Article</button>
     </div>
 
     <div v-else>
-      <button v-on:click="showArticle = true">Article</button>
+      <button v-on:click="listOut">Article</button>
+    </div>
+
+    <div class="createArticle" v-if="createArticle == true">
+      <form @submit.prevent="Confirm">
+        <lable for="input-fields">Article ID</lable>
+        <textarea
+          v-bind:class="{
+            'input-fields': !idrror,
+            'error-input-fields': idError,
+          }"
+          rows="10"
+          v-model="idInput"
+        />
+        <lable for="input-fields">Title</lable>
+        <textarea
+          v-bind:class="{
+            'input-fields': !titleError,
+            'error-input-fields': titleError,
+          }"
+          rows="10"
+          v-model="titleInput"
+        />
+        <lable for="input-fields">Source</lable>
+        <textarea
+          v-bind:class="{
+            'input-fields': !sourceError,
+            'error-input-fields': sourceError,
+          }"
+          rows="10"
+          v-model="sourceInput"
+        />
+        <lable for="input-fields">Country</lable>
+        <textarea
+          v-bind:class="{
+            'input-fields': !countryError,
+            'error-input-fields': CountryError,
+          }"
+          rows="10"
+          v-model="countryInput"
+        />
+        <lable for="new-text">Text</lable>
+        <textarea
+          v-bind:class="{
+            'input-fields': !textError,
+            'error-input-fields': textError,
+          }"
+          rows="10"
+          v-model="contentInput"
+        />
+        <lable for="input-fields">is This a Story?</lable>
+        <textarea
+          v-bind:class="{
+            'input-fields': !storyError,
+            'error-input-fields': storyError,
+          }"
+          rows="10"
+          v-model="booleanInput"
+        />
+        <button v-on:click="createArticle = false">Confirm</button>
+      </form>
     </div>
   </div>
 </template>
@@ -33,19 +93,41 @@ export default {
   components: {},
   data: function () {
     return {
-      showArticle: false,
-    };
-  },
+      articles: null,
+      //button control
 
-  computed: {
-    articles() {
-      return this.$store.state.articles;
-    },
+      showArticle: false,
+      createArticle: false,
+      //input fields
+      id: "",
+      title: "",
+      source: "",
+      content: "",
+      country: "",
+      isStory: "",
+
+      //Errorm
+      titleError: false,
+      sourceError: false,
+      textError: false,
+      storyError: false,
+      idError: false,
+      countryError: false,
+    };
   },
 
   methods: {
     listOut() {
       this.showArticle = true;
+      this.$axios
+      .get("https://unmask.hrithviksood.me/articles/")
+      .then(function(response){
+      console.log(response);
+      });
+    },
+
+    addNewArticle() {
+      //confirm creation
     },
   },
 };
@@ -96,5 +178,16 @@ button:hover {
 }
 .editbox {
   background: #262829;
+}
+
+.input-fields {
+  background-color: rgb(41, 40, 40);
+  width: 40vw;
+  max-height: 2vw;
+  font-size: 1rem;
+  display: block;
+}
+.selection_box {
+  background-color: #6c7986;
 }
 </style>
