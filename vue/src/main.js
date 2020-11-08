21cc6b10f61d7c77692b9b3b847ce0e64fd69468
hsoo6373 <hsoo6373@uni.sydney.edu.au>
2020-11-08 18:38:24 +1100

    Added vue cookies and element ui

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,10 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import { router } from './routes.js'
 import { store } from './store/store.js'
-import vuetify from './plugins/vuetify';
-import VueTypedJs from 'vue-typed-js';
+import vuetify from './plugins/vuetify'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import VueTypedJs from 'vue-typed-js'
+import VueCookies from 'vue-cookies'
+import axios from 'axios'
 
 Vue.use(VueTypedJs);
+Vue.use(VueCookies);
+Vue.use(ElementUI);
+Vue.prototype.$axios = axios;
 Vue.config.productionTip = false;
 
 new Vue({
