037b2e203c7210fc9bb12cbf1ab9d0a134c702d4
Ray-inthebox <47978716+Ray-inthebox@users.noreply.github.com>
2020-11-09 01:16:03 +0800

    created->mounted

--- vue/src/store/store.js
+++ vue/src/store/store.js
@@ -38,9 +38,6 @@ export const store = new Vuex.Store({
     setArticles(state, axiosResponse) {
       state.articles = axiosResponse;
     },
-    setLikeStatus(state, axiosResponse) {
-      state.likeStatus = axiosResponse;
-    },
   },
   
   actions: {
