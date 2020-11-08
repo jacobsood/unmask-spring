2ab4f750c20ef940928f4b4328b8f483a46f906c
hsoo6373 <hsoo6373@uni.sydney.edu.au>
2020-11-08 16:33:52 +1100

    Added new routes for filtering

--- vue/src/routes.js
+++ vue/src/routes.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import LandingPage from "./components/LandingPage.vue";
 import HomePage from "./components/HomePage.vue";
-import IndividualArticle from "./components/IndividualArticle.vue";
 import HistoryPage from "./components/HistoryPage.vue";
 import PreferencePage from "./components/PreferencePage.vue";
 import LoginPage from "./components/LoginPage.vue";
@@ -14,14 +13,12 @@ export const router = new VueRouter({
   mode: "history",
   routes: [
     { name: "landing", path: "/", component: LandingPage },
-    { name: "home", path: "/home", component: HomePage },
-    { name: "tag", path: "/home/:tag", component: HomePage },
-    { name: "country", path: "/home/:country", component: HomePage },
-    {
-      name: "article",
-      path: "/home/:tag/:article",
-      component: IndividualArticle,
-    },
+    { name: "home", path: "/articles", component: HomePage },
+    { name: "tag", path: "/articles/tag/:tag", component: HomePage },
+    { name: "country", path: "/articles/country/:country", component: HomePage },
+    { name: "author", path: "/articles/author/:author", component: HomePage },
+    { name: "search", path: "/articles/:search", component: HomePage },
+    { name: "adminAddedArticles", path: "/articles/:addedBtAdmin", component: HomePage},
     { name: "history", path: "/user/:id/history", component: HistoryPage },
     { name: "newstory", path: "/newstory", component: NewStory },
     {
