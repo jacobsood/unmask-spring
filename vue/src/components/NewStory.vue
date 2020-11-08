501da1eaee4c73c750d0c08c8891a54ed430715c
hsoo6373 <hsoo6373@uni.sydney.edu.au>
2020-11-09 05:06:43 +1100

    Added on click function to newstory

--- vue/src/components/NewStory.vue
+++ vue/src/components/NewStory.vue
@@ -25,12 +25,13 @@
             <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
             <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
         </svg>
-        <button id="publish-button">Publish</button>
+        <button @click="goToPreviousPage" id="publish-button">Publish</button>
     </form>
   </div>
 </template>
 
 <script>
+import { router } from '../routes'
 export default {
   name: 'NewStory',
   data: function() {
@@ -82,6 +83,9 @@ export default {
       if(!this.errorsOnPage){
         console.log("Clear to submit");
       }
+    },
+    goToPreviousPage: function() {
+      router.push({ name: "home" });
     },
 
     isTagsOk(){
